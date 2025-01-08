7a4e6e1778fb44de6d25bb7f13a57997a2da7986
Kazi Emran <kaziemran425@gmail.com>
2025-01-08 15:27:36 +0600

    ecommarce

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,44 @@
 <template>
  <div>
   <div class="row" >
-    <div class="col">ff</div>
-    <div class="col" >ss</div>
+    <div class="col-9 q-pa-xl border ">
+      <div class=" text-h2 text-bold">
+        Sneakers For Woman
+      </div>
+      <div class="text-weight-thin text-caption" >
+        These can include things such as website crashes, slow loading times, poor customer service, and more. However, there are ways to overcome these challenges and succeed in the world of ecommerce.
+      </div>
+          <div class="row">
+          <div class="col-2">
+            <div class="text-h6">price:</div>
+            <div class="text-weight-thin text-caption">$50.60</div>
+          </div>
+          <div class="col-2">
+            <div class="text-h6">Color:</div>
+              <q-avatar size="20px" font-size="10px" color="grey-3" text-color="white"/>
+            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
+            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
+            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
+          </div>
+
+          <div class="col-5 q-px-md">
+            <div>size</div>
+                  <q-btn color="white"label="9" class="text-black"/>
+                  <q-btn color="white"label="10" class="text-black"/>
+                  <q-btn color="white"label="11" class="text-black"/>
+                  <q-btn color="white"label="12" class="text-black"/>
+                  <q-btn color="white"label="13" class="text-red"/>
+
+
+
+
+               </div>
+              </div>
+        </div>
+
+
+
+    <div class="col-3 " >ss</div>
   </div>
  </div>
 </template>
