c6f4b2a95840eab5f1ebe9f3cb642f1c74c807b4
Kazi Emran <kaziemran425@gmail.com>
2025-01-09 15:56:06 +0600

    ecommarce

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,133 +4,111 @@
       <q-toolbar>
         <q-img
           src="https://wpocean.com/html/tf/pengu/assets/images/logo.svg"
-          :ratio="3/1"
+          :ratio="3 / 1"
           spinner-color="primary"
           spinner-size="5px"
           width="90px"
           height="25px"
         />
         <q-space />
-        <div class="q-pa-md text-red" >
-          <q-btn flat no-focus
-           label="HOME">
-        <q-menu
-          transition-show="scale"
-          transition-hide="scale"
-        >
-          <q-list style="min-width: 100px">
-            <q-item clickable>
-              <q-item-section>HOME STYEL 1</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>HOME STYEL 2</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>HOME STYEL 3</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>HOME STYEL 4</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-      <q-btn flat no-focus
-           label="SHOPE">
-        <q-menu
-          transition-show="scale"
-          transition-hide="scale"
-        >
-        </q-menu>
-      </q-btn>
-      <q-btn flat no-focus
-           label="PAGES">
-        <q-menu
-          transition-show="scale"
-          transition-hide="scale"
-        >
-          <q-list style="min-width: 100px">
-            <q-item clickable>
-              <q-item-section>About</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>cart</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>wishlist</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>checkpot</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>product</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>product single</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>Order</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>Login</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>404 Error</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-      <q-btn flat no-focus
-           label="BLOG">
-        <q-menu
-          transition-show="scale"
-          transition-hide="scale"
-        >
-          <q-list style="min-width: 100px">
-            <q-item clickable>
-              <q-item-section>Blog right sider</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>Blog left sider</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>Blog fullwidth</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>Blog details</q-item-section>
-            </q-item>
-
-          </q-list>
-        </q-menu>
-      </q-btn>
-      <q-btn flat no-focus
-           label="CONTACT US">
-        <q-menu
-          transition-show="scale"
-          transition-hide="scale"
-        >
-          <q-list style="min-width: 100px">
-            <q-item clickable>
-              <q-item-section>Having fun</q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section>Crazy for transitions</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>Mind blown</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
+        <div class="q-pa-md text-red">
+          <q-btn flat no-focus label="HOME">
+            <q-menu transition-show="scale" transition-hide="scale">
+              <q-list style="min-width: 100px">
+                <q-item clickable>
+                  <q-item-section>HOME STYEL 1</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>HOME STYEL 2</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>HOME STYEL 3</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>HOME STYEL 4</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn flat no-focus label="SHOPE">
+            <q-menu transition-show="scale" transition-hide="scale"> </q-menu>
+          </q-btn>
+          <q-btn flat no-focus label="PAGES">
+            <q-menu transition-show="scale" transition-hide="scale">
+              <q-list style="min-width: 100px">
+                <q-item clickable>
+                  <q-item-section>About</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>cart</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>wishlist</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>checkpot</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>product</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>product single</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Order</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Login</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>404 Error</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn flat no-focus label="BLOG">
+            <q-menu transition-show="scale" transition-hide="scale">
+              <q-list style="min-width: 100px">
+                <q-item clickable>
+                  <q-item-section>Blog right sider</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Blog left sider</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Blog fullwidth</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Blog details</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn flat no-focus label="CONTACT US">
+            <q-menu transition-show="scale" transition-hide="scale">
+              <q-list style="min-width: 100px">
+                <q-item clickable>
+                  <q-item-section>Having fun</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Crazy for transitions</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Mind blown</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
         </div>
 
-         <q-space />
+        <q-space />
 
         <div>
-          <q-btn icon="shopping_basket" class="q-mx-sm"  color="red" round/>
+          <q-btn icon="shopping_basket" class="q-mx-sm" color="red" round />
           <q-btn icon="search" class="q-mx-sm" color="red" round />
           <q-btn icon="person" class="q-mx-sm" color="red" round />
         </div>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,52 +1,182 @@
 <template>
- <div>
-  <div class="row" >
-    <div class="col-9 q-pa-xl border ">
-      <div class=" text-h2 text-bold">
-        Sneakers For Woman
-      </div>
-      <div class="text-weight-thin text-caption" >
-        These can include things such as website crashes, slow loading times, poor customer service, and more. However, there are ways to overcome these challenges and succeed in the world of ecommerce.
-      </div>
-          <div class="row">
+  <div>
+    <div class="row flex-center q-px-xl">
+      <div class="col-7 q-pa-xl border">
+        <div class="text-h2 text-bold">Sneakers For Woman</div>
+        <div class="text-weight-thin text-caption">
+          These can include things such as website crashes, slow loading times,
+          poor customer service, and more. However, there are ways to overcome
+          these challenges and succeed in the world of ecommerce.
+        </div>
+        <div class="row q-py-lg">
           <div class="col-2">
             <div class="text-h6">price:</div>
             <div class="text-weight-thin text-caption">$50.60</div>
           </div>
           <div class="col-2">
             <div class="text-h6">Color:</div>
-              <q-avatar size="20px" font-size="10px" color="grey-3" text-color="white"/>
-            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
-            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
-            <q-avatar size="20px" font-size="10px" color="brown-3" text-color="white"/>
+            <q-avatar
+              size="20px"
+              font-size="10px"
+              color="grey-3"
+              text-color="white"
+              class="q-mr-sm"
+            />
+            <q-avatar
+              size="20px"
+              font-size="10px"
+              color="brown-3"
+              text-color="white"
+              class="q-mr-sm"
+            />
+            <q-avatar
+              size="20px"
+              font-size="10px"
+              color="purple-11"
+              text-color="white"
+              class="q-mr-sm"
+            />
+            <q-avatar
+              size="20px"
+              font-size="10px"
+              color="green-9"
+              text-color="white"
+            />
           </div>
 
-          <div class="col-5 q-px-md">
-            <div>size</div>
-                  <q-btn color="white"label="9" class="text-black"/>
-                  <q-btn color="white"label="10" class="text-black"/>
-                  <q-btn color="white"label="11" class="text-black"/>
-                  <q-btn color="white"label="12" class="text-black"/>
-                  <q-btn color="white"label="13" class="text-red"/>
+          <div class="col-5">
+            <div class="text-h6">size</div>
 
-
-
-
-               </div>
-              </div>
+            <q-btn color="white" label="9" class="text-black q-mr-sm" />
+            <q-btn color="white" label="10" class="text-black q-mr-sm" />
+            <q-btn color="white" label="11" class="text-black q-mr-sm" />
+            <q-btn color="white" label="12" class="text-black q-mr-sm" />
+            <q-btn color="white" label="13" class="text-red" />
+          </div>
         </div>
+      </div>
 
-
-
-    <div class="col-3 " >ss</div>
+      <div class="col-5 q-py-md">
+        <div class="flex flex-center q-pa-md">
+          <q-img
+            src="https://wpocean.com/html/tf/pengu/assets/images/slider/8.png"
+            :ratio="14 / 13"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row q-px-xl">
+      <div class="q-px-lg col">
+        <q-btn
+          color="red"
+          label="50% off"
+          class="q-mr-lg"
+          style="width: 200px; height: 60px"
+          outline
+          dense
+        />
+        <q-btn
+          text-color="white"
+          label="Shoop Now"
+          style="width: 200px; height: 60px"
+          outline
+          dense
+          class="bg-red"
+        />
+      </div>
+    </div>
+    <!-- seccond page -->
+    <div class="q-py-xl">
+      <div class="text-center text-h4">New Arraival</div>
+      <div class="text-center text-h5 text-weight-thin text-caption">
+        hear is our new arraival products that you may like
+      </div>
+      <div class="q-pa-md text-center flex flex-center">
+        <q-separator style="width: 100px" color="red"></q-separator>
+      </div>
+    </div>
+    <div class="row q-px-lg">
+      <div class="col q-pa-md">
+        <q-card class="my-card">
+          <q-img
+            src="https://wpocean.com/html/tf/pengu/assets/images/product-single/1.jpg"
+            spinner-size="70px"
+          />
+          <div class="q-pa-md flex flex-center">
+            <div class="text-bold text-h6 flex flex-center">
+              Long Sleeve Tops
+            </div>
+            <div class="row q-px-md flex justyfy-center items-center">
+              <div class="col q-pa-md" style="text-decoration: line-through">
+                $85.50
+              </div>
+              <div class="col text-red">$70.30</div>
+            </div>
+          </div>
+        </q-card>
+      </div>
+      <div class="col q-pa-md">
+        <q-card class="my-card">
+          <q-img
+            src="https://wpocean.com/html/tf/pengu/assets/images/product-single/2.jpg"
+            spinner-color="primary"
+            spinner-size="70px"
+          />
+          <div class="q-pa-md flex flex-center">
+            <div class="text-bold text-h6">white wedding shoe</div>
+            <div class="row q-px-md flex justyfy-center items-center">
+              <div class="col q-pa-md" style="text-decoration: line-through">
+                $150.50
+              </div>
+              <div class="col text-red">$120.50</div>
+            </div>
+          </div>
+        </q-card>
+      </div>
+      <div class="col q-pa-md">
+        <q-card class="my-card">
+          <q-img
+            src="https://wpocean.com/html/tf/pengu/assets/images/product-single/3.jpg"
+            spinner-color="primary"
+            spinner-size="70px"
+          />
+          <div class="q-pa-md flex flex-center">
+            <div class="text-bold text-h6">Long chain Whit Lockel</div>
+            <div class="row q-px-md flex justyfy-center items-center">
+              <div class="col q-pa-md" style="text-decoration: line-through">
+                $85.50
+              </div>
+              <div class="col text-red">$70.30</div>
+            </div>
+          </div>
+        </q-card>
+      </div>
+      <div class="col q-pa-md">
+       <q-card class="my-card">
+        <q-img
+          src="https://wpocean.com/html/tf/pengu/assets/images/product-single/4.jpg"
+          spinner-color="primary"
+          spinner-size="70px"
+        />
+        <div class="q-pa-md flex flex-center">
+            <div class="text-bold text-h6">Winter Jacket</div>
+            <div class="row q-px-md flex justyfy-center items-center">
+              <div class="col q-pa-md" style="text-decoration: line-through">
+                $100.50
+              </div>
+              <div class="col text-red">$80.30</div>
+            </div>
+          </div>
+       </q-card>
+      </div>
+    </div>
   </div>
- </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
